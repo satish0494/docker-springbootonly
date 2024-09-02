aa922797621da7b61a48450d3d3be620194adef7
Satish <manepallikumar3258@gmail.com>
2024-09-02 22:40:05 +0530

    initial commit for spring boot

---
+++ src/test/java/com/docker/springboot/Docker_springBoot/DockerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.springboot.Docker_springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
