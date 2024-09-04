a9aee55d7a94262352a3fd7dc81ebd9de0027560
Satish <manepallikumar3258@gmail.com>
2024-09-04 21:15:32 +0530

    added docker-compose.yml changes

---
+++ src/main/java/com/docker/springboot/Docker_springBoot/rest/Book.java
@@ -0,0 +1,52 @@
+package com.docker.springboot.Docker_springBoot.rest;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer bookId;
+
+	private String bookName;
+
+	private String authorName;
+
+	private Double bookPrice;
+	
+	public Integer getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(Integer bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public Double getBookPrice() {
+		return bookPrice;
+	}
+
+	public void setBookPrice(Double bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+}
--- src/main/java/com/docker/springboot/Docker_springBoot/rest/DockerRestController.java
+++ src/main/java/com/docker/springboot/Docker_springBoot/rest/DockerRestController.java
@@ -1,9 +1,15 @@
 package com.docker.springboot.Docker_springBoot.rest;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping()
 public class DockerRestController {
 
+	@Autowired
+	private DockerRepo repo;
+	
+	@Autowired 
+	private BookJPARepo bookRepo;
 	
 	private Logger logger = LoggerFactory.getLogger(DockerRestController.class);
 
@@ -37,4 +48,22 @@ public class DockerRestController {
 		logger.info("***** ***************************** *****");
 		return msg;
 	}
+	
+	@GetMapping("/get/{id}")
+	public String getNameById(@PathVariable int id) {
+		return repo.getNameById(id);
+	}
+	
+	@GetMapping("/getAllBooks")
+	public List<Book> getAllBooks(){
+		logger.info("getAllBooks started");
+		return bookRepo.findAll();
+	}
+	
+	@PostMapping("/addBook")
+	public Book addBook(@RequestBody Book book) {
+		logger.info("addBook started");
+		return bookRepo.save(book);
+	}
 }
+
