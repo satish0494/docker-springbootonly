2924231bb176043c24eda3913d81f044b4413618
Satish <manepallikumar3258@gmail.com>
2024-09-04 22:21:01 +0530

    removed jdbc info

--- src/main/java/com/docker/springboot/Docker_springBoot/rest/DockerRestController.java
+++ src/main/java/com/docker/springboot/Docker_springBoot/rest/DockerRestController.java
@@ -5,9 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping()
 public class DockerRestController {
 
-	@Autowired
-	private DockerRepo repo;
+//	@Autowired
+//	private DockerRepo repo;
 	
 	@Autowired 
 	private BookJPARepo bookRepo;
@@ -49,10 +47,10 @@ public class DockerRestController {
 		return msg;
 	}
 	
-	@GetMapping("/get/{id}")
-	public String getNameById(@PathVariable int id) {
-		return repo.getNameById(id);
-	}
+//	@GetMapping("/get/{id}")
+//	public String getNameById(@PathVariable int id) {
+//		return repo.getNameById(id);
+//	}
 	
 	@GetMapping("/getAllBooks")
 	public List<Book> getAllBooks(){
