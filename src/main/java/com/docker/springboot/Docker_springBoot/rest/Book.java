38fd7f6efa474225709ea923a0058b7596777d6d
Satish <manepallikumar3258@gmail.com>
2024-09-04 21:26:42 +0530

    converting from jakarta to javax

--- src/main/java/com/docker/springboot/Docker_springBoot/rest/Book.java
+++ src/main/java/com/docker/springboot/Docker_springBoot/rest/Book.java
@@ -1,9 +1,9 @@
 package com.docker.springboot.Docker_springBoot.rest;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Book {
